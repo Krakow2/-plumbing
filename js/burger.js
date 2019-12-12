7e7ab2a34e2184b7173c905e939794403ed6dd1a
Krakow2 <50423999+Krakow2@users.noreply.github.com>
2019-12-12 04:44:34 +0300

    Add files via upload

---
+++ js/burger.js
@@ -0,0 +1,16 @@
+$('.burger-menu').on('click', function (e) {
+  e.preventDefault;
+  $(this).toggleClass('burger-menu_active');
+});
+const MenuB = document.querySelector('.burger-menu')
+modalB = document.querySelector('.container-bg'),
+closeB = document.querySelector('.burger-menu_active')
+
+MenuB.addEventListener('click', () => {
+  $('div.container-bg').toggleClass('container-bg_active')
+});
+
+// $('.burger-menu').on('click', function (e) {
+//   e.preventDefault;
+//   $(this).toggleClass('burger-menu_active');
+// });
